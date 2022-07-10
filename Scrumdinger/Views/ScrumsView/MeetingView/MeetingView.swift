b53e153dc96dbdd1588a8fb3c7dd5af8dda5b063
shin-carpediem <buru.aoshin@gmail.com>
2022-07-10 09:47:24 +0900

    add AVfaundation

--- Scrumdinger/Views/ScrumsView/MeetingView/MeetingView.swift
+++ Scrumdinger/Views/ScrumsView/MeetingView/MeetingView.swift
@@ -1,8 +1,12 @@
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
@@ -17,6 +21,10 @@ struct MeetingView: View {
             .foregroundColor(scrum.theme.accentColor)
             .onAppear {
                 scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+                scrumTimer.speakerChangedAction = {
+                    player.seek(to: .zero)
+                    player.play()
+                }
                 scrumTimer.startScrum()
             }
             .onDisappear {
