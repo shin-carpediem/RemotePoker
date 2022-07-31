553e49f93ecafea979bd9bfb7ac38093bcccfee5
shin-carpediem <buru.aoshin@gmail.com>
2022-07-31 10:26:19 +0900

    fix bug

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -2,35 +2,9 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-    @StateObject private var store = ScrumStore()
-    @State private var errorWrapper: ErrorWrapper?
-
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                IndexView(scrums: $store.scrums) {
-                    Task {
-                        do {
-                            // discardableResultsave属性を関数に付したので、戻り値は無視できる。
-                            try await ScrumStore.save(scrums: store.scrums)
-                        } catch {
-                            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
-                        }
-                    }
-                }
-            }
-            .task {
-                do {
-                    store.scrums = try await ScrumStore.load()
-                } catch {
-                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
-                }
-            }
-            .sheet(item: $errorWrapper, onDismiss: {
-                store.scrums = DailyScrum.sampleData
-            }) { wrapper in
-                ErrorView(errorWrapper: wrapper)
-            }
+            IndexView()
         }
     }
 }
