e192cc9e7eca27519327137ad446e926b92a569d
shin-carpediem <buru.aoshin@gmail.com>
2022-08-18 23:21:05 +0900

    add subcollection

--- Scrumdinger/Models/PlanningPokerModel/RoomModel.swift
+++ Scrumdinger/Models/PlanningPokerModel/RoomModel.swift
@@ -5,30 +5,51 @@ class RoomModel: Identifiable {
     let id = String(Int.random(in: 1000..<9999))
     
     private(set) var usersId: [String] = []
-//    private(set) var cardList = [EstimateNumberSetModel.sampleData]
+    private(set) var cardList = [EstimateNumberSetModel.sampleData]
     
     // MARK: - Method
     
     func createRoom() {
         roomCollection.document(id).setData([
             "id": id,
-            "usersId": usersId,
-//            "cardList": cardList
-        ])
+            "usersId": usersId
+        ]) { error in
+            print("Error writing document: \(String(describing: error))")
+        }
+        let cardListSubCollection = roomCollection.document(id).collection("cardList")
+        cardList.forEach {
+            let cardListId = $0.id
+            cardListSubCollection.document(cardListId).setData([
+                "id": cardListId,
+                // TODO: SwiftUIのColorは型でもないしFirestoreで保存できるデータではない
+//                "color": $0.color
+            ])
+            $0.numberSet.forEach {
+                let eachCardId = $0.id
+                cardListSubCollection.document(cardListId).collection("numberSet").document(eachCardId).setData([
+                    "id": eachCardId,
+                    "number": $0.number
+                ])
+            }
+        }
     }
     
     func addUserToRoom(_ userId: String) {
         usersId.append(userId)
         roomCollection.document(id).updateData([
             "usersId": usersId
-        ])
+        ]) { error in
+            print("Error writing document: \(String(describing: error))")
+        }
     }
     
     func removeUserFromRoom(_ userId: String) {
         usersId.removeAll(where: {$0 == userId})
         roomCollection.document(id).updateData([
-            "usersId": userId
-        ])
+            "usersId": usersId
+        ]) { error in
+            print("Error writing document: \(String(describing: error))")
+        }
     }
     
     // MARK: - Private
