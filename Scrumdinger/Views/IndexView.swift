7c003a5f4c6e21038f32bf9da240de2b2a6760c3
shin-carpediem <buru.aoshin@gmail.com>
2022-08-28 00:37:33 +0900

    comment out tutorial

--- Scrumdinger/Views/IndexView.swift
+++ Scrumdinger/Views/IndexView.swift
@@ -9,49 +9,52 @@ struct IndexView: View {
     // MARK: - Body
     
     var body: some View {
-        TabView {
-            NavigationView {
-                EnterRoomView()
-            }
-            .tabItem {
-                VStack {
-                    Image(systemName: "lanyardcard")
-                    Text("Planning Poker")
-                }
-            }
-            .tag(1)
-            NavigationView {
-                ScrumsView(scrums: $store.scrums) {
-                    Task {
-                        do {
-                            // discardableResultsave属性を関数に付したので、戻り値は無視できる。
-                            try await ScrumStore.save(scrums: store.scrums)
-                        } catch {
-                            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
-                        }
-                    }
-                }
-            }
-            .task {
-                do {
-                    store.scrums = try await ScrumStore.load()
-                } catch {
-                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
-                }
-            }
-            .sheet(item: $errorWrapper, onDismiss: {
-                store.scrums = DailyScrum.sampleData
-            }) { wrapper in
-                ErrorView(errorWrapper: wrapper)
-            }
-            .tabItem {
-                VStack {
-                    Image(systemName: "timer")
-                    Text("Scrum Meeting")
-                }
-            }
-            .tag(2)
+        NavigationView {
+            EnterRoomView()
         }
+//        TabView {
+//            NavigationView {
+//                EnterRoomView()
+//            }
+//            .tabItem {
+//                VStack {
+//                    Image(systemName: "lanyardcard")
+//                    Text("Planning Poker")
+//                }
+//            }
+//            .tag(1)
+//            NavigationView {
+//                ScrumsView(scrums: $store.scrums) {
+//                    Task {
+//                        do {
+//                            // discardableResultsave属性を関数に付したので、戻り値は無視できる。
+//                            try await ScrumStore.save(scrums: store.scrums)
+//                        } catch {
+//                            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
+//                        }
+//                    }
+//                }
+//            }
+//            .task {
+//                do {
+//                    store.scrums = try await ScrumStore.load()
+//                } catch {
+//                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
+//                }
+//            }
+//            .sheet(item: $errorWrapper, onDismiss: {
+//                store.scrums = DailyScrum.sampleData
+//            }) { wrapper in
+//                ErrorView(errorWrapper: wrapper)
+//            }
+//            .tabItem {
+//                VStack {
+//                    Image(systemName: "timer")
+//                    Text("Scrum Meeting")
+//                }
+//            }
+//            .tag(2)
+//        }
     }
 }
 
