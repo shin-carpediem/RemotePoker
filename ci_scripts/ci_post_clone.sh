6fff427599c3a05f978c22768bab8ca72ec24518
shin-carpediem <buru.aoshin@gmail.com>
2023-03-04 11:28:15 +0900

    ビルドバージョンの更新

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,4 +5,5 @@ brew install carthage
 brew install swift-format
 
 # build packages installed by Cathage and make cache for upcoming app build
-carthage update --platform iOS --cache-builds --use-xcframeworks --project-directory ../Users/aokishinichiro/develop/Scrumdinger
+PROJECT_ROOT_DIR=$(cd ..;pwd)
+carthage update --platform iOS --cache-builds --use-xcframeworks --project-directory ${PROJECT_ROOT_DIR}
