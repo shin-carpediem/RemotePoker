a4a138d3abe61899b01d00c12c8f88f4889fc993
shin-carpediem <buru.aoshin@gmail.com>
2023-03-04 16:10:53 +0900

    ダウンロード先のファイルパスを修正

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -6,7 +6,7 @@
 # (少なくとも)9.6.0以降において FirebaseAnalytics に含まれるライブラリ PromisesObjC.xcframework は、
 # CarthageでインストールするためにFirebase公式が用意したバイナリ中には含まれないため、手動インストールする。
 # Archive
-PROMISES_OBJC_ARCHIVE_FILE=https://github.com/shin-carpediem/PromisesObjC.xcframework/archive/refs/tags/1.0.0.zip
+PROMISES_OBJC_ARCHIVE_FILE=https://github.com/shin-carpediem/PromisesObjC.xcframework/releases/latest/download/PromisesObjC.xcframework-1.0.0.zip
 # Script
 echo "🏃 PromisesObjC.xcframework has started been installing."
 PROJECT_ROOT_DIR=$(cd ..;pwd)
