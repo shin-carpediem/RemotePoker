f6dbea002c2d2f59961b54fb6b9fd1f7aa68c045
shin-carpediem <buru.aoshin@gmail.com>
2023-03-21 23:13:58 +0900

    権限付与を適切にする

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,15 +5,6 @@
 
 # (少なくとも)9.6.0以降において FirebaseAnalytics に含まれるライブラリ PromisesObjC.xcframework は、
 # CarthageでインストールするためにFirebase公式が用意したバイナリ中には含まれないため、手動インストールする。
-PROMISES_OBJC_ARCHIVE_URL=https://github.com/shin-carpediem/PromisesObjC.xcframework/archive/refs/tags/1.0.0.zip
-SCRIPT_DIR=$(cd $(dirname $0);pwd)
-PROMISES_OBJC_ARCHIVE_FILE=${SCRIPT_DIR}/PromisesObjC.zip
-PROJECT_ROOT_DIR=$(cd ..;pwd)
-PROMISES_OBJC_INSTALL_DIR=${PROJECT_ROOT_DIR}/Carthage/Build
-
-echo "🏃 Installing PromisesObjC.xcframework."
-curl -L ${PROMISES_OBJC_ARCHIVE_URL} --output ${PROMISES_OBJC_ARCHIVE_FILE}
-unzip -q -o ${PROMISES_OBJC_ARCHIVE_FILE} -d ${PROMISES_OBJC_INSTALL_DIR}
-rm -r ${PROMISES_OBJC_ARCHIVE_FILE}
-mv ${PROMISES_OBJC_INSTALL_DIR}/PromisesObjC.xcframework-1.0.0 ${PROMISES_OBJC_INSTALL_DIR}/PromisesObjC.xcframework
-echo "✅ Completed installing PromisesObjC.xcframework."
+cd ../bin
+chmod u+rwx install_promisesobjc.sh
+./install_promisesobjc.sh
