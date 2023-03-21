9e6a52d11f5217b06e80a900f6334a3106082f4f
shin-carpediem <buru.aoshin@gmail.com>
2023-03-21 23:06:28 +0900

    ビルドバージョンのインクリメント

--- bin/install_promisesobjc.sh
+++ bin/install_promisesobjc.sh
@@ -8,9 +8,9 @@ PROMISES_OBJC_ARCHIVE_FILE=${SCRIPT_DIR}/PromisesObjC.zip
 PROJECT_ROOT_DIR=$(cd ..;pwd)
 PROMISES_OBJC_INSTALL_DIR=${PROJECT_ROOT_DIR}/Carthage/Build
 
-echo "🏃 PromisesObjC.xcframework has started been installing."
+echo "🏃 Installing PromisesObjC.xcframework."
 curl -L ${PROMISES_OBJC_ARCHIVE_URL} --output ${PROMISES_OBJC_ARCHIVE_FILE}
 unzip -q -o ${PROMISES_OBJC_ARCHIVE_FILE} -d ${PROMISES_OBJC_INSTALL_DIR}
 rm -r ${PROMISES_OBJC_ARCHIVE_FILE}
 mv ${PROMISES_OBJC_INSTALL_DIR}/PromisesObjC.xcframework-1.0.0 ${PROMISES_OBJC_INSTALL_DIR}/PromisesObjC.xcframework
-echo "✅ PromisesObjC.xcframework got completed been installing."
+echo "✅ Completed installing PromisesObjC.xcframework."
