61a57d699c8e9423d102fc5c9ed21f74b046406d
shin-carpediem <buru.aoshin@gmail.com>
2023-03-22 08:51:29 +0900

    環境変数をローカルとリモートで適用させる

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,3 +7,10 @@ brew install swift-format
 # build packages installed by Cathage and make cache for upcoming app build
 PROJECT_ROOT_DIR=$(cd ..;pwd)
 carthage bootstrap --platform iOS --cache-builds --use-xcframeworks --no-use-binaries --project-directory ${PROJECT_ROOT_DIR}
+
+# apply environment variables set on Xcode Cloud
+GOOGLESERVICE_INFO_PRODUCTION_FILE_PATH="../RemotePoker/GoogleService-Info.plist"
+GOOGLESERVICE_INFO_DEVELOPMENT_FILE_PATH="../RemotePoker/GoogleService-Info-Dev.plist"
+
+sed -i -e "s/GOOGLESERVICE_INFO_PRODUCTION_API_KEY/${GOOGLESERVICE_INFO_PRODUCTION_API_KEY}/g" "${GOOGLESERVICE_INFO_PRODUCTION_FILE_PATH}"
+sed -i -e "s/GOOGLESERVICE_INFO_DEVELOPMENT_API_KEY/${GOOGLESERVICE_INFO_DEVELOPMENT_API_KEY}/g" "${GOOGLESERVICE_INFO_DEVELOPMENT_FILE_PATH}"
