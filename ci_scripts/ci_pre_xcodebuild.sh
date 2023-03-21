1bcb5adf766590aefc3e0bc72d5f89d7edbf98fa
shin-carpediem <buru.aoshin@gmail.com>
2023-03-21 15:40:13 +0900

    スクリプトの修正

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,5 +5,4 @@
 
 # (少なくとも)9.6.0以降において FirebaseAnalytics に含まれるライブラリ PromisesObjC.xcframework は、
 # CarthageでインストールするためにFirebase公式が用意したバイナリ中には含まれないため、手動インストールする。
-cd ../bin
-./install_promisesobjc
+source ../bin/install_promisesobjc
