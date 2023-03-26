eb49fefbf3d51eca8fdaf0a05a05e1457515f189
shin-carpediem <buru.aoshin@gmail.com>
2023-03-26 14:27:32 +0900

    スクリプトの修正

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -9,5 +9,5 @@ PROJECT_ROOT_DIR=$(cd ..;pwd)
 carthage bootstrap --platform iOS --cache-builds --use-xcframeworks --no-use-binaries --project-directory ${PROJECT_ROOT_DIR}
 
 # apply environment variables set on Xcode Cloud
-GOOGLESERVICE_INFO_PRODUCTION_FILE_PATH="../RemotePoker/GoogleService-Info.plist"
-plutil -replace "GOOGLESERVICE_INFO_PRODUCTION_API_KEY" -string ${GOOGLESERVICE_INFO_PRODUCTION_API_KEY} ${GOOGLESERVICE_INFO_PRODUCTION_FILE_PATH}
+GOOGLESERVICE_INFO_PRODUCTION_FILE_PATH="${PROJECT_ROOT_DIR}/GoogleService-Info.plist"
+plutil -replace "API_KEY" -string ${GOOGLESERVICE_INFO_PRODUCTION_API_KEY} ${GOOGLESERVICE_INFO_PRODUCTION_FILE_PATH}
