69bd09634d303325bbce9c4571e6341c63638610
shin-carpediem <buru.aoshin@gmail.com>
2023-07-01 15:47:14 +0900

    順番を入れ替える

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,7 +5,11 @@ brew install swift-format
 
 # apply environment variables set on Xcode Cloud
 PROJECT_ROOT_DIR=$(cd ..;pwd)
-GOOGLESERVICE_INFO_PRODUCTION_FILE_PATH="${PROJECT_ROOT_DIR}/RemotePoker/GoogleService-Info.plist"
+
+## development environment
 GOOGLESERVICE_INFO_DEVELOPMENT_FILE_PATH="${PROJECT_ROOT_DIR}/RemotePoker/GoogleService-Info-Dev.plist"
-plutil -replace "API_KEY" -string ${GOOGLESERVICE_INFO_PRODUCTION_API_KEY} ${GOOGLESERVICE_INFO_PRODUCTION_FILE_PATH}
 plutil -replace "API_KEY" -string ${GOOGLESERVICE_INFO_DEVELOPMENT_API_KEY} ${GOOGLESERVICE_INFO_DEVELOPMENT_FILE_PATH}
+
+## production environment
+GOOGLESERVICE_INFO_PRODUCTION_FILE_PATH="${PROJECT_ROOT_DIR}/RemotePoker/GoogleService-Info.plist"
+plutil -replace "API_KEY" -string ${GOOGLESERVICE_INFO_PRODUCTION_API_KEY} ${GOOGLESERVICE_INFO_PRODUCTION_FILE_PATH}
